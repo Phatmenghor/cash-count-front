cbadbb9353757752e2338d216cd3b124c7407684
GIT-Phat <menghor.phat@cambodiapostbank.com.kh>
2024-09-30 14:14:18 +0700

    add-fun-token

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-700 text-white fixed top-0 left-0 right-0 z-50 shadow-md">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <h1 className="text-xl font-bold">My App</h1>
+        <h1 className="text-xl font-bold">Case count</h1>
         <div className="flex items-center space-x-4">
           <Link href="/user-management" className="hover:underline">
             Manage Users
